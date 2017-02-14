8025a3004234b8da863159bc2fc7baaf017dcf21
CompliantArms <travklein13@gmail.com>
2017-02-13 17:57:57 -0800

    updates

--- js/brads-javascript.js
+++ js/brads-javascript.js
@@ -50,3 +50,110 @@ $(".contact-form").on("submit", function(event) {
       }
   });
 })
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  }
+}
+
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+
+
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+$(".accordion").click(function() {
+  $(".accordion").removeClass("active");
+
+  $(".panel").each(function() {
+    this.style.maxHeight = null;
+  });
+});
+
+
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    //changeImage('images/maglock.png');
+    $("#imageReplace").attr('src', this.dataset.photo);
+
+    this.classList.add("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      //close
+  	  panel.style.maxHeight = null;
+    } else {
+      // open
+  	  panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  }
+}
+
+function changeImage(element) {
+document.getElementById('imageReplace').src = element;
+}
+
+var vid = document.getElementById("bgvid");
+var pauseButton = document.querySelector("#polina button");
+
+if (window.matchMedia('(prefers-reduced-motion)').matches) {
+    vid.removeAttribute("autoplay");
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+}
+
+function vidFade() {
+  vid.classList.add("stopfade");
+}
+
+vid.addEventListener('ended', function()
+{
+// only functional if "loop" is removed
+vid.pause();
+// to capture IE10
+vidFade();
+});
+
+
+pauseButton.addEventListener("click", function() {
+  vid.classList.toggle("stopfade");
+  if (vid.paused) {
+    vid.play();
+    pauseButton.innerHTML = "Pause";
+  } else {
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+  }
+})
